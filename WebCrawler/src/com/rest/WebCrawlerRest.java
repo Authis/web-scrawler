e370d8fd39af5e5724b06b44724ff2ce2a25eca5
Authis <authis.sundar@gmail.com>
2016-12-22 02:43:02 +0530

    Update WebCrawlerRest.java
    Done URL validation and changed return type to JSONArray from stringBuilder. 
    As Jsoup accept only http /https urls , the validation for same has been done

--- WebCrawler/src/com/rest/WebCrawlerRest.java
+++ WebCrawler/src/com/rest/WebCrawlerRest.java
@@ -15,6 +15,8 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
 import org.jsoup.Connection;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
@@ -31,42 +33,95 @@ public class WebCrawlerRest {
 	@POST
 	@Path("/StartCrawle")
 	@Consumes(MediaType.APPLICATION_JSON)
-	@Produces(MediaType.TEXT_HTML)
-	public String WCRESTService(String url) {
+	@Produces(MediaType.APPLICATION_JSON)
+	public JSONArray WCRESTService(String url) {
  
 		  
 		System.out.println("Input URL >> "+url);
- 		/*StringBuilder sb = new StringBuilder();
-		URLConnection urlConn = null;
-		InputStreamReader in = null;*/
 		String urlValue = "";
-		String arr[] = url.split("://");
-		System.out.println(">>>>"+arr[1]);
-		String domineStr = arr[1];
-		 StringBuilder JSON =new StringBuilder();
-		try {
-		 
-			Connection connection = Jsoup.connect(url);
-			connection.timeout(20000);
-			Document doc = connection.get();
-			Elements questions = doc.select("a");			
-			JSON.append("{'urllist': [");			
-			for(Element link: questions){				 
-				urlValue = link.attr("href");
-				if(urlValue.indexOf(domineStr) >= 0){
-					//System.out.println("11111111 :  " +link.attr("href") );	
-					 JSON.append("{'DOM':'NATIVE','VALUE':"+urlValue+"}");
-				}else{					
-					//System.out.println("22222222 :  " +link.attr("href") );
-					JSON.append("{'DOM':'OTHER','VALUE':'"+urlValue+"'}");
-				} 
-				JSON.append(",");
-			}			
-		    JSON.append("]}");
-		} catch (Exception e) {
-			e.printStackTrace();
+		String arr[] = null;
+		String error = null;
+		String domineStr = "";
+		if(url.indexOf("http://www.") >= 0){                  // for url type http://wiprodigital.com
+			arr = url.split("://www.");	
+			domineStr = arr[1]; 
+		}else if(url.indexOf("http://") >=0){         // for url type http://www.wiprodigital.com
+			 arr = url.split("://");	
+			 domineStr = arr[1]; 
+		}else if(url.indexOf("https://") >= 0){           // for url type https://wiprodigital.com
+			arr = url.split("s://");
+			domineStr = arr[1]; 
+		}else if(url.indexOf("https://www.") >=0){          // for url type https://www.wiprodigital.com
+			arr = url.split("s://www");
+			domineStr = arr[1]; 
+		}else if(url.indexOf("www.") >=0){                // for url type www.wiprodigital.com
+			/*arr = url.split("\\.");
+			domineStr = arr[1]; */
+			error = "Please enter valid Url with HTTP";
+		}else if(url.indexOf(".com") >=0){                // for url type wiprodigital.com
+			/*arr = url.split("\\.");
+			domineStr = arr[0]; */
+			error = "Please enter valid Url with HTTP";
+		}else{
+			error = "Please enter valid Url  with HTTP";
 		}
-		return JSON.toString();
+		 
+		System.out.println(">>>>"+arr);
+		  
+		  
+		 JSONObject obj = null;
+		 
+		 JSONArray list = new JSONArray();
+		 
+		 if(error == null){
+		 
+		 
+				try { 
+					Connection connection = Jsoup.connect(url);
+					connection.timeout(1000*6);
+					Document doc = connection.get();
+					Elements questions = doc.select("a");
+					 
+					for(Element link: questions){	 
+						obj = new JSONObject();
+						urlValue = link.attr("href");
+						if(urlValue.indexOf(domineStr) >= 0){
+							System.out.println("11111111 :  " +urlValue );					   
+							obj.put("DOM","NATIVE");
+							obj.put("VALUE",urlValue);
+							
+							list.add(obj); 
+						}else if((urlValue.indexOf("http://") >= 0 || urlValue.indexOf("https://") >= 0)&& urlValue.indexOf(domineStr) < 0){					
+							System.out.println("22222222 :  " +urlValue);					  
+							obj.put("DOM","OTHER");
+							obj.put("VALUE",urlValue);
+							list.add(obj); 
+						} 
+					}  
+						
+					/*}else{
+						obj.put("ERROR",error);				 
+					}			
+				   */ 
+				   System.out.println("Final : "+list);
+				} catch (Exception e) {
+					obj = new JSONObject();
+					obj.put("DOM","ERROR");	
+					obj.put("VALUE","Unknown Host Exception Occured. Please enter valid Url");
+					list.add(obj); 
+					e.printStackTrace();
+					
+				}
+		 }else{
+			    obj = new JSONObject();
+				obj.put("DOM","ERROR");		
+				obj.put("VALUE",error);		
+				list.add(obj); 
+			}
+		
+		
+		return list;
+		 
 	}
 
 }
