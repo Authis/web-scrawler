7dcc14ec5d93051480e3be9c623f9ea11f301255
Authis <authis.sundar@gmail.com>
2016-12-22 02:45:11 +0530

    Update script.js
    Code modified to assign Output to an array  and bind the JSON to the HTML

--- WebCrawler/WebContent/js/script.js
+++ WebCrawler/WebContent/js/script.js
@@ -4,17 +4,29 @@
 
 var app = angular.module('wcApp', []);
 	app.controller('startCrawel', function($scope,$http,$log) {
-      alert("====");
+           //alert("====");
+           $scope.txtURL = "http://wiprodigital.com";
 		$scope.loadUrls = function(){
 			
 			var url = $scope.txtURL;
             
-			alert(url);
+			//alert("URL is: "+url);
 			
-			$http
-			.post("http://localhost:8080/WebCrawler/WC/StartCrawle", url)
+			$http.post("http://localhost:8090/WebCrawler/WC/StartCrawle", url)
 	 			.success(function(data, status, headers, config) {
-	 				$log.log("post method is Success" + data);
+	 				$log.log("post method is Success" + JSON.stringify(data));
+	 				    
+	 				    
+	 				
+						$scope.urlValues =data;
+						if($scope.urlValues.length == 1 && $scope.urlValues[0]['DOM'] == 'ERROR'){
+							//alert("1");
+							$scope.error = "Y";
+						}else{
+							//alert("2");
+							$scope.error = "N";
+							$scope.urlValues = data
+						}
 						
 
 					}).error(
