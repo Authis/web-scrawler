975d035980b4fb1b9123d6f9a401d7eb43e4fe6f
Authis <authis.sundar@gmail.com>
2016-12-21 17:08:09 +0530

    Edited the Business logic
    Tried with URLConnection and Buffered reader and modified to Jsoup which having inbuilt HTML querying methods.

--- WebCrawler/src/com/rest/WebCrawlerRest.java
+++ WebCrawler/src/com/rest/WebCrawlerRest.java
@@ -1,8 +1,13 @@
 package com.rest;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.net.URLConnection;
+import java.nio.charset.Charset;
 
 import javax.ws.rs.Consumes;
 import javax.ws.rs.POST;
@@ -10,8 +15,11 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
+import org.jsoup.Connection;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
 
 
@@ -24,15 +32,50 @@ public class WebCrawlerRest {
 	@Path("/StartCrawle")
 	@Consumes(MediaType.APPLICATION_JSON)
 	@Produces(MediaType.TEXT_HTML)
-	public String loginRESTService(String url) {
+	public String WCRESTService(String url) {
  
 		String returnStr = "CCCC";		 
 		System.out.println("Input URL >> "+url);
+ 		/*StringBuilder sb = new StringBuilder();
+		URLConnection urlConn = null;
+		InputStreamReader in = null;*/
+
 		try {
-			 Document doc = Jsoup.connect("http://www.google.com/").get();			 
-				if(doc.text().contains("research")){
-					System.out.println("---"+doc.text());
+		 
+			 Connection connection = Jsoup.connect(url);
+				connection.timeout(20000);
+				Document doc = connection.get();
+				//System.out.println(doc);		 
+				 
+				Elements questions = doc.select("a[href]");
+				for(Element link: questions){
+					System.out.println("All Sub URLS----:  " +link);
+						 
 				}
+			
+			/*URL url1 = new URL(url);
+			urlConn = url1.openConnection();
+			if (urlConn != null)
+				urlConn.setReadTimeout(60 * 1000);
+			if (urlConn != null && urlConn.getInputStream() != null) {
+				in = new InputStreamReader(urlConn.getInputStream(),
+						Charset.defaultCharset());
+				BufferedReader bufferedReader = new BufferedReader(in);
+				if (bufferedReader != null) {
+					int cp;
+					while ((cp = bufferedReader.read()) != -1) {
+						System.out.println(((char) cp);
+					}
+					bufferedReader.close();
+				}
+			}
+		in.close();
+*/
+	             
+	            
+			
+			
+			
 			 
 		} catch (Exception e) {
 			e.printStackTrace();
